fe3bf5fd368ab61e85ab00808cb5545b402210f1
Klubin <clubin2004@gmail.com>
2023-11-04 16:49:14 +0200

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 339);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1086, 470);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.label1.Location = new System.Drawing.Point(251, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(398, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Time Interval (Seconds)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.textBox1.Location = new System.Drawing.Point(12, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(233, 48);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "100";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(369, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(327, 55);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Process View";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.textBox2.Location = new System.Drawing.Point(12, 172);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(1086, 161);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "No Data";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.label3.Location = new System.Drawing.Point(12, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(233, 40);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Process Data";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.button1.Location = new System.Drawing.Point(927, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 48);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Kill";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1110, 821);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private Process selectedProcess;
+        Timer timer;
+        public Form1()
+        {
+            InitializeComponent();
+            UpdateProcessList();
+            timer = new Timer();
+            timer.Interval = int.Parse(textBox1.Text) * 1000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+            listView1.Columns.Add("Process Name", 100);
+            listView1.Columns.Add("Process ID", 70);
+            listView1.Columns.Add("Start Time", 150);
+            listView1.Columns.Add("Total Processor Time", 150);
+            listView1.Columns.Add("Number of Threads", 120);
+            listView1.Columns.Add("Number of Instances", 150);
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timer.Interval = int.Parse(textBox1.Text) * 1000;
+            UpdateProcessList();
+        }
+
+        private void UpdateProcessList()
+        {
+            Process[] processes = Process.GetProcesses();
+            List<ListViewItem> listPr = new List<ListViewItem>();
+            foreach (Process process in processes)
+            {
+                ListViewItem item = new ListViewItem(process.ProcessName);
+                item.SubItems.Add(process.Id.ToString());
+                try
+                {
+                    item.SubItems.Add(process.StartTime.ToString());
+                    item.SubItems.Add(process.TotalProcessorTime.ToString());
+                    item.SubItems.Add(process.Threads.Count.ToString());
+                    item.SubItems.Add(Process.GetProcessesByName(process.ProcessName).Length.ToString());
+                }
+                catch (Win32Exception ex)
+                {
+                    item.SubItems.Add("N/A");
+                    item.SubItems.Add("N/A");
+                    item.SubItems.Add("N/A");
+                    item.SubItems.Add("N/A");
+                }
+                listPr.Add(item);
+            }
+            listView1.Items.Clear();
+            foreach (ListViewItem item in listPr)
+            {
+                listView1.Items.Add(item);
+            }
+        }
+
+        private void buttonSetInterval_Click(object sender, EventArgs e)
+        {
+            int interval;
+            if (int.TryParse(textBox1.Text, out interval))
+            {
+                timer.Interval = interval;
+            }
+        }
+
+        private void listViewProcesses_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem item = listView1.SelectedItems[0];
+                string processName = item.SubItems[0].Text;
+                Process[] processes = Process.GetProcessesByName(processName);
+                if (processes.Length > 0)
+                {
+                    selectedProcess = processes[0];
+                    textBox2.Text = $"Process ID: {selectedProcess.Id}\r\n" +
+                        $"Start Time: {selectedProcess.StartTime}\r\n" +
+                        $"Total Processor Time: {selectedProcess.TotalProcessorTime}\r\n" +
+                        $"Number of Threads: {selectedProcess.Threads.Count}\r\n" +
+                        $"Number of Instances: {processes.Length}\r\n";
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem item = listView1.SelectedItems[0];
+                string processName = item.SubItems[0].Text;
+                Process[] processes = Process.GetProcessesByName(processName);
+                if (processes.Length > 0)
+                {
+                    selectedProcess = processes[0];
+                    string[] data = new string[5];
+                    try
+                    {
+                        data[0] = selectedProcess.Id.ToString();
+                        data[1] = selectedProcess.StartTime.ToString();
+                        data[2] = selectedProcess.TotalProcessorTime.ToString();
+                        data[3] = selectedProcess.Threads.Count.ToString();
+                        data[4] = Process.GetProcessesByName(selectedProcess.ProcessName).Length.ToString();
+                    }
+                    catch (Win32Exception ex)
+                    {
+                        data[0] = selectedProcess.Id.ToString();
+                        data[1] = "N/A";
+                        data[2] = "N/A";
+                        data[3] = "N/A";
+                        data[4] = "N/A";
+                    }
+                    textBox2.Text =
+                        "ID: " + data[0] +
+                        "\r\nStartTime: " + data[1] +
+                        "\r\nTotalProccessorTime: " + data[2] +
+                        "\r\nNumThreads: " + data[3] +
+                        "\r\nNumInstance: " + data[4];
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (selectedProcess != null)
+                {
+                    selectedProcess.Kill();
+                    MessageBox.Show("Процесс успешно завершен.");
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Выбранный процесс отсутствует.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Не удалось завершить процесс: " + ex.Message);
+            }
+        }
+    }
+}
